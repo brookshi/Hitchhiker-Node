9eb2a40f3ae964d8d0ead7789b88e6d1ee23017e
brookshi <iwxiaot@gmail.com>
2017-10-06 18:06:24 +0800

    try duktape

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/brookshi/Hitchhiker-Node/hlog"
-	"github.com/robertkrimen/otto"
+	duktape "gopkg.in/olebedev/go-duktape.v2"
 )
 
 func main() {
@@ -15,26 +15,36 @@ func main() {
 	// client := &client{}
 	// client.Do()
 	hlog.Info.Println("start")
-	vm := otto.New()
+	//vm := otto.New()
+	ctx := duktape.New()
 	hlog.Info.Println("new vm")
 	start := time.Now()
-	v, err := vm.Run(`
-			var responseCode = {code: 200};
-			var responseObj = {success: false, result: {id: "001"}}
-			var tests = {};
-			tests["Status code is 200"] = responseCode.code === 200;
-			tests["request is success"] = responseObj.success;
-			tests["id is correct"] = responseObj.result.id === "001";
-			result = JSON.stringify(tests);
-		
-		`)
 
+	err := ctx.PevalString("const a = 1; result = `asdf${a}fdsa`;")
+	// ctx.PevalString(`var responseCode = {code: 200};
+	// 		var responseObj = {success: false, result: {id: "001"}}
+	// 		var tests = {};
+	// 		tests["Status code is 200"] = responseCode.code === 200;
+	// 		tests["request is success"] = responseObj.success;
+	// 		tests["id is correct"] = responseObj.result.id === "001";
+	// 		result = JSON.stringify(tests);`)
+	// v, err := vm.Run(`
+	// 		var responseCode = {code: 200};
+	// 		var responseObj = {success: false, result: {id: "001"}}
+	// 		var tests = {};
+	// 		tests["Status code is 200"] = responseCode.code === 200;
+	// 		tests["request is success"] = responseObj.success;
+	// 		tests["id is correct"] = responseObj.result.id === "001";
+	// 		result = JSON.stringify(tests);
+
+	// 	`)
+	result := ctx.GetNumber(-1)
 	duration := time.Now().Sub(start)
 	hlog.Info.Println(duration.Nanoseconds())
 	hlog.Info.Println("end")
-	hlog.Info.Println(v)
+	hlog.Info.Println(result)
 	hlog.Info.Println(err)
-	hlog.Info.Println(vm.Get("result"))
+	// hlog.Info.Println(vm.Get("result"))
 
 	time.Sleep(time.Duration(1) * time.Hour)
 }
